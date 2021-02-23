c4272103bdb2305e019b0f50c3a5fc090fa57456
Christof Wendenius <christof.wendenius@kit.edu>
2021-02-23 14:50:45 +0100

    gradients

--- main.py
+++ main.py
@@ -22,38 +22,35 @@ def get_device(sim_local, wires, analytic=False):
     return dev
 
 
-def my_circuit(dev, wires, layers, params, rotations, dropouts):
-    @qml.qnode(dev)
-    def variational_circuit():
+def variational_circuit(params, wires, layers, rotations, dropouts):
+    for w in range(wires):
+        qml.RY(np.pi/4, wires=w) 
+    r = 0
+    for l in range(layers):
         for w in range(wires):
-            qml.RY(np.pi/4, wires=w) 
-        r = 0
-        for l in range(layers):
-            for w in range(wires):
-                if dropouts[l][w] == 1:
-                    r += 1
-                    continue
-                if rotations[r] == 0:
-                    rotation = qml.RX
-                elif rotations[r] == 1:
-                    rotation = qml.RY
-                else:
-                    rotation = qml.RZ
+            if dropouts[l][w] == 1:
                 r += 1
-                rotation(params[l][w], wires=w)
-        
-            for w in range(wires-1):
-                qml.CZ(wires=[w, w+1])
+                continue
+            if rotations[r] == 0:
+                rotation = qml.RX
+            elif rotations[r] == 1:
+                rotation = qml.RY
+            else:
+                rotation = qml.RZ
+            r += 1
+            rotation(params[l][w], wires=w)
+    
+        for w in range(wires-1):
+            qml.CZ(wires=[w, w+1])
 
-        H = np.zeros((2 ** wires, 2 ** wires))
-        H[0, 0] = 1
-        wirelist = [i for i in range(wires)]
-        return qml.expval(qml.Hermitian(H, wirelist))
-    return variational_circuit
+    H = np.zeros((2 ** wires, 2 ** wires))
+    H[0, 0] = 1
+    wirelist = [i for i in range(wires)]
+    return qml.expval(qml.Hermitian(H, wirelist))
 
 
-def cost(dev, wires, layers, params, rotations, dropouts):
-    return my_circuit(dev, wires, layers, params, rotations, dropouts)()
+def cost(circuit, params, wires, layers, rotations, dropouts):
+    return circuit(params, wires, layers, rotations, dropouts)
 
 
 def determine_dropout(params, dropout, epsilon=0.01, factor=0.2, difference=0):
@@ -83,7 +80,7 @@ def determine_dropout(params, dropout, epsilon=0.01, factor=0.2, difference=0):
     return new_dropout
 
 
-def train_circuit(wires=5, layers=5, steps=500, sim_local=True, use_dropout=True):
+def train_circuit(wires=5, layers=5, steps=500, sim_local=True, use_dropout=False):
     dev = get_device(sim_local, wires=wires)
 
     rotation_choices = [0, 1, 2]
@@ -98,19 +95,26 @@ def train_circuit(wires=5, layers=5, steps=500, sim_local=True, use_dropout=True
     opt = qml.GradientDescentOptimizer(stepsize=0.01)
     last_cost = None
 
+    circuit = qml.QNode(variational_circuit, dev)
+
+    grad = qml.grad(circuit, argnum=0)
+    gradient = grad(params, wires=wires, layers=layers, rotations=rotations, dropouts=dropouts)
+
     for step in range(steps):
-        new_cost = cost(dev, wires, layers, params, rotations, dropouts)
+        new_cost = cost(circuit, params, wires, layers, rotations, dropouts)
+        gradient = grad(params, wires=wires, layers=layers, rotations=rotations, dropouts=dropouts)
+
         if last_cost is None:
             last_cost = new_cost
         difference = last_cost - new_cost
-        
-        print(step, new_cost)
-        params = opt.step(lambda p: cost(dev, wires, layers, p, rotations, dropouts), params)
+
+        params = opt.step(lambda p: cost(circuit, p, wires, layers, rotations, dropouts), params)
         # determine new dropouts based on new parameter values
         if use_dropout:
             dropouts = determine_dropout(params, dropouts, difference=difference)
         last_cost = new_cost
 
+        print("Step: {:4d} | Cost: {: .5f} | Gradient Variance: {: .9f}".format(step, new_cost, np.var(gradient)))
 
 if __name__ == "__main__":
     train_circuit()
