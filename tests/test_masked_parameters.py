44564168a2ef159f08e7085bcec04b13665c562e
Eileen Kuehn <eileen.kuehn@kit.edu>
2021-02-25 11:07:29 +0100

    added more tests

--- tests/test_masked_parameters.py
+++ tests/test_masked_parameters.py
@@ -27,11 +27,8 @@ def test_perturbation(perturbation):
         factor = 3
     for i in range(3):
         mp.perturb(i + 1)
-        print(f"current i {i}")
-        print(mp.mask)
         assert pnp.sum(mp.mask) == (i + 1) * factor
         mp.perturb(i + 1, mode=PerturbationMode.REMOVE)
-        print(mp.mask)
         assert pnp.sum(mp.mask) == 0
 
 
@@ -42,10 +39,51 @@ def test_negative_perturbation(perturbation):
         mp.perturb(-1)
 
 
-# (PerturbationMode.INVERT, PerturbationMode.REMOVE),
-# (PerturbationMode.INVERT, PerturbationMode.INVERT),
-# (PerturbationMode.ADD, PerturbationMode.INVERT),
-# (PerturbationMode.REMOVE, PerturbationMode.ADD),
+@pytest.mark.parametrize('perturbation', list(PerturbationAxis))
+def test_perturbation_remove_add(perturbation):
+    mp = _create_masked_parameter(perturbation_axis=perturbation)
+    factor = 1
+    if perturbation != PerturbationAxis.RANDOM:
+        factor = 3
+
+    maximum_size = mp.mask.size if perturbation == PerturbationAxis.RANDOM else len(
+        mp.mask)
+    for amount in [random.randrange(maximum_size), 0, maximum_size, maximum_size + 1]:
+        mp.perturb(amount=amount, mode=PerturbationMode.REMOVE)
+        assert pnp.sum(mp.mask) == 0
+        mp.perturb(amount=amount, mode=PerturbationMode.ADD)
+        assert pnp.sum(mp.mask) == min(amount, maximum_size) * factor
+        mp.reset()
+
+
+@pytest.mark.parametrize('perturbation', list(PerturbationAxis))
+@pytest.mark.parametrize('mode', [(PerturbationMode.ADD, PerturbationMode.INVERT), (PerturbationMode.INVERT, PerturbationMode.INVERT)])
+def test_perturbation_mode(perturbation, mode):
+    mp = _create_masked_parameter(perturbation_axis=perturbation)
+
+    maximum_size = mp.mask.size if perturbation == PerturbationAxis.RANDOM else len(
+        mp.mask)
+    for amount in [0, maximum_size, maximum_size + 1]:
+        mp.perturb(amount=amount, mode=mode[0])
+        mp.perturb(amount=amount, mode=mode[1])
+        assert pnp.sum(mp.mask) == 0
+
+
+@pytest.mark.parametrize('perturbation', list(PerturbationAxis))
+def test_perturbation_invert_remove(perturbation):
+    mp = _create_masked_parameter(perturbation_axis=perturbation)
+    factor = 1
+    if perturbation != PerturbationAxis.RANDOM:
+        factor = 3
+
+    maximum_size = mp.mask.size if perturbation == PerturbationAxis.RANDOM else len(
+        mp.mask)
+    for amount in [random.randrange(maximum_size), 0, maximum_size, maximum_size + 1]:
+        mp.perturb(amount=amount, mode=PerturbationMode.INVERT)
+        reversed_amount = pnp.sum(mp.mask)
+        mp.perturb(amount=reversed_amount, mode=PerturbationMode.REMOVE)
+        assert pnp.sum(mp.mask) == 0
+
 
 @pytest.mark.parametrize('perturbation', list(PerturbationAxis))
 def test_perturbation_add_remove(perturbation):
@@ -56,7 +94,6 @@ def test_perturbation_add_remove(perturbation):
 
     maximum_size = mp.mask.size if perturbation == PerturbationAxis.RANDOM else len(mp.mask)
     for amount in [random.randrange(maximum_size), 0, maximum_size, maximum_size + 1]:
-        print(f"amount {amount}, factor {factor}, perturbation {perturbation}")
         mp.perturb(amount=amount, mode=PerturbationMode.ADD)
         assert pnp.sum(mp.mask) == min(amount, maximum_size) * factor
         mp.perturb(amount=amount, mode=PerturbationMode.REMOVE)
