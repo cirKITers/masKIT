d51a7ffd2de9518d03afcd4dc919ff192e72b1e2
Christof Wendenius <christof.wendenius@kit.edu>
2021-02-26 17:41:22 +0100

    1000 vs 3000

--- plotting.py
+++ plotting.py
@@ -9,29 +9,28 @@ def load_data(file):
 
 
 def plot_cost_stupid(data):
-    data_length = 0
-
     for d in data:
         costs = d["result"]["costs"]
-        if data_length == 0:
-            data_length = len(costs)
         final_costs = d["result"]["final_cost"]
         steps = []
         values = []
-        for step in list(costs.keys())[:data_length]:
+        for step, value in costs.items():
             steps.append(int(step))
-            values.append(costs[step])
+            values.append(value)
         steps.append(steps[-1]+5)
         values.append(final_costs)
     
+        if d["call"][2]["use_dropout"]:
+            steps = [s*3 for s in steps]
+
         plt.plot(steps, values)
     
     plt.show()
 
 
 if __name__ == "__main__":
-    files = ["logs/W5_L20_DOtrue.json",
-             "logs/W5_L20_DOfalse.json"]
+    files = ["logs/W5_L10_DOtrue.json",
+             "logs/W5_L10_DOfalse.json"]
 
     data = []
 
