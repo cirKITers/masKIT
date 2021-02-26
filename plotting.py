d8716626c9ef483d8f33f2e5c7e778f52555d30f
Eileen Kuehn <eileen.kuehn@kit.edu>
2021-02-26 17:21:50 +0100

    added final costs

--- plotting.py
+++ plotting.py
@@ -1,6 +1,7 @@
 import json
 import matplotlib.pyplot as plt
 
+
 def load_data(file):
     with open(file) as json_file:
         data = json.load(json_file)
@@ -11,11 +12,15 @@ def plot_cost(data):
 
     for d in data:
         costs = d["result"]["costs"]
+        final_costs = d["result"]["final_cost"]
         steps = []
         values = []
         for step, value in costs.items():
             steps.append(int(step))
             values.append(value)
+        steps.append(steps[-1]+5)
+        values.append(final_costs)
+
     
         plt.plot(steps, values)
     
