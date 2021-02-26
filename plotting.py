487c8ac9bfc6339fa37a48e4fba63b852cbabf2f
Eileen Kuehn <eileen.kuehn@kit.edu>
2021-02-26 17:27:00 +0100

    same length plot ignoring ensembles

--- plotting.py
+++ plotting.py
@@ -8,19 +8,21 @@ def load_data(file):
     return data
 
 
-def plot_cost(data):
+def plot_cost_stupid(data):
+    data_length = 0
 
     for d in data:
         costs = d["result"]["costs"]
+        if data_length == 0:
+            data_length = len(costs)
         final_costs = d["result"]["final_cost"]
         steps = []
         values = []
-        for step, value in costs.items():
+        for step in list(costs.keys())[:data_length]:
             steps.append(int(step))
-            values.append(value)
+            values.append(costs[step])
         steps.append(steps[-1]+5)
         values.append(final_costs)
-
     
         plt.plot(steps, values)
     
@@ -28,12 +30,12 @@ def plot_cost(data):
 
 
 if __name__ == "__main__":
-    files = ["logs/W5_L20_DOfalse.json",
-             "logs/W5_L20_DOtrue.json"]
+    files = ["logs/W5_L20_DOtrue.json",
+             "logs/W5_L20_DOfalse.json"]
 
     data = []
 
     for f in files:
         data.append(load_data(f))
 
-    plot_cost(data)
+    plot_cost_stupid(data)
