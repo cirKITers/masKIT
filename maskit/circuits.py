7319c15e8fc683269851c16d629f5d7fee94641d
Eileen Kuehn <eileen.kuehn@kit.edu>
2021-03-15 15:58:28 +0100

    refactored creation of circuits

--- maskit/circuits.py
+++ maskit/circuits.py
@@ -2,7 +2,7 @@ import pennylane as qml
 from pennylane import numpy as np
 
 
-def variational_circuit(params, wires, layers, rotations, dropouts):
+def basic_variational_circuit(params, wires, layers, rotations, dropouts):
     for wire in range(wires):
         qml.RY(np.pi / 4, wires=wire)
     r = -1
@@ -23,30 +23,26 @@ def variational_circuit(params, wires, layers, rotations, dropouts):
             qml.CZ(wires=[wire, wire + 1])
         for wire in range(1, wires - 1, 2):
             qml.CZ(wires=[wire, wire + 1])
+
+
+def variational_circuit(params, wires, layers, rotations, dropouts):
+    basic_variational_circuit(
+        params=params,
+        wires=wires,
+        layers=layers,
+        rotations=rotations,
+        dropouts=dropouts,
+    )
     return qml.probs(wires=range(wires))
 
 
 def iris_circuit(params, data, wires, layers, rotations, dropouts):
     qml.templates.embeddings.AngleEmbedding(features=data, wires=range(4), rotation="X")
-
-    for wire in range(wires):
-        qml.RY(np.pi / 4, wires=wire)
-    r = -1
-    for layer in range(layers):
-        for wire in range(wires):
-            r += 1
-            if dropouts[layer][wire] is True:
-                continue
-            if rotations[r] == 0:
-                rotation = qml.RX
-            elif rotations[r] == 1:
-                rotation = qml.RY
-            else:
-                rotation = qml.RZ
-            rotation(params[layer][wire], wires=wire)
-
-        for wire in range(0, wires - 1, 2):
-            qml.CZ(wires=[wire, wire + 1])
-        for wire in range(1, wires - 1, 2):
-            qml.CZ(wires=[wire, wire + 1])
+    basic_variational_circuit(
+        params=params,
+        wires=wires,
+        layers=layers,
+        rotations=rotations,
+        dropouts=dropouts,
+    )
     return qml.probs(wires=[0, 1])
