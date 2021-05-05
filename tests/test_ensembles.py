3b7ec011ec7d714e4ca8e03ee783b78ab71966fe
Eileen Kuehn <eileen.kuehn@kit.edu>
2021-05-05 20:10:28 +0200

    added further unittests for ensembles

--- tests/test_ensembles.py
+++ tests/test_ensembles.py
@@ -71,10 +71,6 @@ QHACK = {
 }
 
 
-# TODO: remove explicit tests for private methods
-# TODO: add tests for step
-
-
 class TestEnsemble:
     def test_init(self):
         ensemble = Ensemble(dropout={})
@@ -87,14 +83,14 @@ class TestEnsemble:
         mp = _create_circuit(size)
         ensemble = Ensemble(dropout=definition)
         branches = ensemble._branch(masked_circuit=mp)
+        assert branches
         assert len(definition) == len(branches)
 
     def test_ensemble_unintialised_branches(self):
         mp = _create_circuit(3)
         ensemble = Ensemble(dropout=None)
         branches = ensemble._branch(masked_circuit=mp)
-        assert len(branches) == 1
-        assert list(branches.values())[0] == mp
+        assert branches is None
 
     @pytest.mark.parametrize("dropout", [QHACK, RANDOM])
     def test_ensemble_step(self, dropout):
@@ -128,11 +124,41 @@ class TestIntervalEnsemble:
             IntervalEnsemble(dropout={}, interval=0)
         assert IntervalEnsemble(dropout={}, interval=1)
 
-    def test_branch(self):
-        assert False
+    @pytest.mark.parametrize("dropout", [QHACK, RANDOM])
+    def test_step(self, dropout):
+        interval = 3
+        mp = _create_circuit(3, layer_size=2)
+        optimizer = ExtendedGradientDescentOptimizer()
+        circuit = qml.QNode(_variational_circuit, _device(mp.wire_mask.size))
+        simple_ensemble = Ensemble(dropout=None)
+        interval_ensemble = IntervalEnsemble(dropout=dropout, interval=interval)
 
-    def test_check_interval(self):
-        assert False
+        def cost_fn(params, masked_circuit=None):
+            return _cost(
+                params,
+                circuit,
+                masked_circuit,
+            )
+
+        # train for three steps and assert that interval ensemble is better
+        simple_costs = []
+        simple_mp = mp.copy()
+        for _ in range(interval):
+            simple_mp, _name, cost, _gradient = simple_ensemble.step(
+                simple_mp, optimizer, cost_fn, step_count=1
+            )
+            simple_costs.append(cost)
+        interval_mp = mp.copy()
+        for i in range(interval - 1):
+            interval_mp, _name, cost, _gradient = interval_ensemble.step(
+                interval_mp, optimizer, cost_fn, step_count=1
+            )
+            assert simple_costs[i] == cost
+        # last step should be better
+        interval_mp, _name, cost, _gradient = interval_ensemble.step(
+            interval_mp, optimizer, cost_fn, step_count=1
+        )
+        assert simple_costs[-1] > cost
 
 
 class TestAdaptiveEnsemble:
@@ -143,13 +169,42 @@ class TestAdaptiveEnsemble:
         assert ensemble
         assert ensemble.perturb is False
 
-    def test_branch(self):
-        mp = _create_circuit(3)
-        ensemble = AdaptiveEnsemble(size=2, dropout={"center": {}}, epsilon=0.1)
-        ensemble.perturb = True
-        branch = ensemble._branch(masked_circuit=mp)
-        assert ensemble.perturb is False
-        assert len(branch) == 1
+    @pytest.mark.parametrize("dropout", [QHACK, RANDOM])
+    def test_step(self, dropout):
+        random.seed(1234)
+        pnp.random.seed(1234)
+        mp = _create_circuit(3, layer_size=2)
+        optimizer = ExtendedGradientDescentOptimizer()
+        circuit = qml.QNode(_variational_circuit, _device(mp.wire_mask.size))
+        simple_ensemble = Ensemble(dropout=None)
+        adaptive_ensemble = AdaptiveEnsemble(dropout=dropout, size=3, epsilon=0.01)
+
+        def cost_fn(params, masked_circuit=None):
+            return _cost(
+                params,
+                circuit,
+                masked_circuit,
+            )
+
+        # train for four steps and assert that interval ensemble is better
+        simple_costs = []
+        simple_mp = mp.copy()
+        for _ in range(4):
+            simple_mp, _name, cost, _gradient = simple_ensemble.step(
+                simple_mp, optimizer, cost_fn, step_count=1
+            )
+            simple_costs.append(cost)
+        adaptive_mp = mp.copy()
+        for i in range(3):
+            adaptive_mp, _name, cost, _gradient = adaptive_ensemble.step(
+                adaptive_mp, optimizer, cost_fn, step_count=1
+            )
+            assert simple_costs[i] == cost
+        # last step should be better
+        adaptive_mp, _name, cost, _gradient = adaptive_ensemble.step(
+            adaptive_mp, optimizer, cost_fn, step_count=1
+        )
+        assert simple_costs[-1] > cost
 
 
 class TestEnsembleUseCases:
