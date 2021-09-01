d6b35e93d826bf885e2ef9bee3d7bfc9f7c6774d
Christof Wendenius <christof.wendenius@kit.edu>
2021-09-01 14:07:53 +0200

    cross entropy loss is now divided by batch size

--- maskit/utils.py
+++ maskit/utils.py
@@ -12,11 +12,12 @@ def cross_entropy(predictions, targets, epsilon=1e-12):
     """
     Computes cross entropy between targets (encoded as one-hot vectors)
     and predictions.
-    Input: predictions (N, k) ndarray
-           targets (N, k) ndarray
+    Input: predictions (N, k) or (k) ndarray
+           targets with the same dimensions as predictions
     Returns: scalar
     """
+    assert(predictions.shape == targets.shape)
     predictions = np.clip(predictions, epsilon, 1.0 - epsilon)
-    N = predictions.shape[0]
+    N = predictions.shape[0] if len(predictions.shape) == 2 else 1
     ce = -np.sum(targets * np.log(predictions + 1e-9)) / N
     return ce
