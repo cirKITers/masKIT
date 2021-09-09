55fd728ab799362d717de8e5cae0bd4188bf27a5
Eileen Kuehn <eileen.kuehn@kit.edu>
2021-09-09 09:26:23 +0200

    use log_loss to validate result

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sklearn.metrics import log_loss
 
 from maskit.utils import cross_entropy
 
@@ -6,6 +7,6 @@ from maskit.utils import cross_entropy
 def test_cross_entropy():
     predictions = np.array([[0.25, 0.25, 0.25, 0.25], [0.01, 0.01, 0.01, 0.96]])
     targets = np.array([[0, 0, 0, 1], [0, 0, 0, 1]])
-    expected_value = 0.71355817782  # Correct answer
+    expected_value = log_loss(targets, predictions)
     calculated_value = cross_entropy(predictions=predictions, targets=targets)
     assert np.isclose(expected_value, calculated_value)
