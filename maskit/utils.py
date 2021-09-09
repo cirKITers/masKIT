21eb8e946f88e341586ff4aa7f15acca749a69e0
Eileen Kuehn <eileen.kuehn@kit.edu>
2021-09-09 12:21:21 +0200

    Documentation, test and normalization for cross entropy calculation (#45)
    * add documentation for cross entropy
    
    * test cross entropy calculation with sklearn log_loss
    
    * add normalization for predictions

--- maskit/utils.py
+++ maskit/utils.py
@@ -8,16 +8,38 @@ def check_params(train_params):
     assert isinstance(train_params["optimizer"], ExtendedOptimizers)
 
 
-def cross_entropy(predictions, targets, epsilon=1e-12):
+def cross_entropy(
+    predictions: np.ndarray, targets: np.ndarray, epsilon: float = 1e-15
+) -> float:
     """
-    Computes cross entropy between targets (encoded as one-hot vectors)
-    and predictions.
-    Input: predictions (N, k) or (k) ndarray
-           targets with the same dimensions as predictions
-    Returns: scalar
+    Cross entropy calculation between :py:attr:`targets` (encoded as one-hot vectors)
+    and :py:attr:`predictions`. Predictions are normalized to sum up to `1.0`.
+
+    .. note::
+
+        The implementation of this function is based on the discussion on
+        `StackOverflow <https://stackoverflow.com/a/47398312/10138546>`_.
+
+        Due to ArrayBoxes that are required for automatic differentiation, we currently
+        use this implementation instead of implementations provided by sklearn for
+        example.
+
+    :param predictions: Predictions in same order as targets. In case predictions for
+        several samples are given, the weighted cross entropy is returned.
+    :param targets: Ground truth labels for supplied samples.
+    :param epsilon: Amount to clip predictions as log is not defined for `0` and `1`.
     """
-    assert(predictions.shape == targets.shape)
+    assert (
+        predictions.shape == targets.shape
+    ), f"Shape of predictions {predictions.shape} must match targets {targets.shape}"
+    current_sum = np.sum(predictions, axis=predictions.ndim - 1)
+
+    if predictions.ndim == 1:
+        sample_count = 1
+        predictions = predictions / current_sum
+    else:
+        sample_count = predictions.shape[0]
+        predictions = predictions / current_sum[:, np.newaxis]
+
     predictions = np.clip(predictions, epsilon, 1.0 - epsilon)
-    N = predictions.shape[0] if len(predictions.shape) == 2 else 1
-    ce = -np.sum(targets * np.log(predictions + 1e-9)) / N
-    return ce
+    return -np.sum(targets * np.log(predictions)) / sample_count
