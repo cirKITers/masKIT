253e30dc94cdec94ceeb4bfe7bfefe260cb64cc8
Niklas Metz <niklasmetz@gmx.de>
2021-09-13 22:24:13 +0200

    Added some documentation to the main load function of the datasets.

--- maskit/datasets/__init__.py
+++ maskit/datasets/__init__.py
@@ -4,7 +4,20 @@ from maskit.datasets.mnist import mnist
 from maskit.datasets.utils import Data
 
 
-def load_data(dataset, **kwargs):
+def load_data(dataset: str, **kwargs):
+    """
+    Returns the data for the requested ``dataset``.
+
+    :param dataset (str): Name of the chosen dataset.
+        Available datasets are: iris, mnist and circles.
+    :param kwargs: Further arguments for dataset selection. Possible arguments:
+        train_size (int): Size of the training dataset
+        test_size (int): Size of the testing dataset
+        shuffle (boolean): if the dataset should be shuffled
+        classes (list[int]): which numbers of the mnist dataset should be included
+        wires (int): number of wires in the circuit
+    :raises ValueError: Raised if a not supported dataset is requested
+    """
     train_size = kwargs.get("train_size", 100)
     test_size = kwargs.get("test_size", 50)
     shuffle = kwargs.get("shuffle", True)
